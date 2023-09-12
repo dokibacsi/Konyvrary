0d133954979fcc9376c23957bca2e30efe7269cc
dokibacsi <kvalcsi02@gmail.com>
2023-09-12 15:07:31 +0200

    fakeadat feltoltes

---
+++ konyvrary/database/factories/CopyFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+use Nette\Utils\Random;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Copy>
+ */
+class CopyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => rand(1, 10),
+            'book_id' => rand(1, 10)
+        ];
+    }
+}
